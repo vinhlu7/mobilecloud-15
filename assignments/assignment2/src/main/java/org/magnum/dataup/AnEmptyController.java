73909f41b4c99c8f6c9b1a5772f3aa5fd162deb9
Vinh <stevenlu7@yahoo.com>
2015-06-30 22:51:04 -0700

    Passed first test case

--- assignments/assignment2/src/main/java/org/magnum/dataup/AnEmptyController.java
+++ assignments/assignment2/src/main/java/org/magnum/dataup/AnEmptyController.java
@@ -17,26 +17,67 @@
  */
 package org.magnum.dataup;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.magnum.dataup.model.Video;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Controller
 public class AnEmptyController {
 
 	/**
 	 * You will need to create one or more Spring controllers to fulfill the
-	 * requirements of the assignment. If you use this file, please rename it
-	 * to something other than "AnEmptyController"
+	 * requirements of the assignment. If you use this file, please rename it to
+	 * something other than "AnEmptyController"
 	 * 
-	 * 
-		 ________  ________  ________  ________          ___       ___  ___  ________  ___  __       
-		|\   ____\|\   __  \|\   __  \|\   ___ \        |\  \     |\  \|\  \|\   ____\|\  \|\  \     
-		\ \  \___|\ \  \|\  \ \  \|\  \ \  \_|\ \       \ \  \    \ \  \\\  \ \  \___|\ \  \/  /|_   
-		 \ \  \  __\ \  \\\  \ \  \\\  \ \  \ \\ \       \ \  \    \ \  \\\  \ \  \    \ \   ___  \  
-		  \ \  \|\  \ \  \\\  \ \  \\\  \ \  \_\\ \       \ \  \____\ \  \\\  \ \  \____\ \  \\ \  \ 
-		   \ \_______\ \_______\ \_______\ \_______\       \ \_______\ \_______\ \_______\ \__\\ \__\
-		    \|_______|\|_______|\|_______|\|_______|        \|_______|\|_______|\|_______|\|__| \|__|
-                                                                                                                                                                                                                                                                        
 	 * 
 	 */
+
+	private static final AtomicLong currentId = new AtomicLong(0L);
+
+	private Map<Long, Video> videos = new HashMap<Long, Video>();
+
+	@RequestMapping(value = "/video", method = RequestMethod.POST)
+	public @ResponseBody Video addVideo(@RequestBody Video v) {
+		checkAndSetId(v);
+		videos.put(v.getId(), v);
+		v.setDataUrl(getDataUrl(v.getId()));
+		return v;
+	}
+
+	@RequestMapping(value = "/video", method = RequestMethod.GET)
+	public @ResponseBody Map<Long, Video> getVideo(){
+		return videos;
+	}
 	
+	private void checkAndSetId(Video entity) {
+		if (entity.getId() == 0) {
+			entity.setId(currentId.incrementAndGet());
+		}
+	}
+
+	private String getDataUrl(long videoId) {
+		String url = getUrlBaseForLocalServer() + "/video/" + videoId + "/data";
+		return url;
+	}
+
+	private String getUrlBaseForLocalServer() {
+		HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder
+				.getRequestAttributes()).getRequest();
+		String base = "http://"
+				+ request.getServerName()
+				+ ((request.getServerPort() != 80) ? ":"
+						+ request.getServerPort() : "");
+		return base;
+	}
 }
